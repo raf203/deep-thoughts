c0063edf7cc4f6777aadb8238da3b023c8ddd035
Rafael Cardoso <rrafa203@gmail.com>
2022-02-11 17:12:26 -0800

    add form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,8 @@ import FriendList from '../components/FriendList';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
+import ThoughtForm from '../components/ThoughtForm';
+
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -19,6 +21,11 @@ const loggedIn = Auth.loggedIn();
   return (
     <main>
   <div className="flex-row justify-space-between">
+  {loggedIn && (
+      <div className="col-12 mb-3">
+        <ThoughtForm />
+      </div>
+    )}
   <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
   {loading ? (
     <div>Loading...</div>
